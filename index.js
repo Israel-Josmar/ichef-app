40acb7b8b5ca11307096ea7d8971238099cb8f8b
Josmar Dias <josmarnet@gmail.com>
2019-08-31 10:29:30 -0300

    add apollo client

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+import React from 'react'
 import { AppRegistry } from 'react-native'
+import { Provider as PaperProvider } from 'react-native-paper'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
 import App from './src/App'
 import { name as appName } from './app.json'
 
-AppRegistry.registerComponent(appName, () => App)
+const client = new ApolloClient({
+  uri: 'http://10.0.2.2:4000/graphql',
+})
+
+const connectedApp = () => (
+  <ApolloProvider client={client}>
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  </ApolloProvider>
+)
+
+AppRegistry.registerComponent(appName, () => connectedApp)
