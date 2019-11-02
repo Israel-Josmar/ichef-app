bdd2380b3038b3ccd4f918e422eaa2c43468e4d3
Luiz Fellype <lfcassago@gmail.com>
2019-11-02 05:19:49 -0300

    fix: query and renderItem function in FlatList

--- src/App.js
+++ src/App.js
@@ -5,19 +5,31 @@ import { FlatList, Text } from 'react-native'
 import { CardDish } from './components'
 
 const QUERY_DISHES = gql`
-  query GetDishes {
-    dishes {
-      id
-      name
-      category
-      imageUrl
-      description
+  query GetDishes($first: Int!) {
+    dishes(first: $first) {
+      totalCount
+      edges {
+        node {
+          id
+          name
+          category
+          description
+          imageUrl
+        }
+      }
+      pageInfo {
+        hasNextPage
+        startCursor
+        endCursor
+      }
     }
   }
 `
 
 const App = () => {
-  const { loading, error, data } = useQuery(QUERY_DISHES)
+  const { loading, error, data } = useQuery(QUERY_DISHES, {
+    variables: { first: 10 },
+  })
 
   if (loading) return <Text>Loading...</Text>
   if (error) return <Text>Error :( -> {JSON.stringify(error, null, 2)}</Text>
@@ -25,8 +37,8 @@ const App = () => {
   return (
     <FlatList
       keyExtractor={item => item.id}
-      data={data.dishes}
-      renderItem={({ item }) => <CardDish key={item.id} dish={item} />}
+      data={data.dishes.edges}
+      renderItem={({ item: { node } }) => <CardDish key={node.id} dish={node} />}
     />
   )
 }
